0ff03c119f847d121d95eb7d558d0af96830a133
PairODice <Jackson.cameron.m@gmail.com>
2021-11-10 18:42:34 -0600

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,6 @@
 import glob
 import cv2
 import os
-import imageio
-import numpy as np
 
 input_dir = "./raw_data"
 out_dir = "./processed_data"
@@ -15,9 +13,8 @@ except Exception:
     print('processed data directory found')
 
 counter = 0
-for j in range(len(files)):
-    for file in glob.glob(files[j] + "/*.jpg")[0:25]:
-        raw_img = cv2.imread(file)
-        processed_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
-        cv2.imwrite(out_dir + "/{:08d}.png".format(counter), processed_img)
-        counter += 1
+for file in glob.glob(input_dir):
+    raw_img = cv2.imread(file)
+    processed_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
+    cv2.imwrite(out_dir + "/{:08d}.png".format(counter), processed_img)
+    counter += 1

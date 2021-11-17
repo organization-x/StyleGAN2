5ce6989fb136ae4e1a80af0c6b9ce83e080bae7f
PairODice <Jackson.cameron.m@gmail.com>
2021-11-17 17:32:28 -0600

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -5,7 +5,7 @@ import os
 input_dir = "./raw_data"
 out_dir = "./processed_data"
 files = glob.glob("./raw_data/images/*")
-IMG_SIZE = 256
+IMG_SIZE = 512
 
 try:
     os.mkdir('./processed_data')
@@ -14,7 +14,10 @@ except Exception:
 
 counter = 0
 for file in glob.glob(input_dir):
-    raw_img = cv2.imread(file)
-    processed_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
-    cv2.imwrite(out_dir + "/{:08d}.png".format(counter), processed_img)
-    counter += 1
+    try:
+        raw_img = cv2.imread(file)
+        processed_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(out_dir + "/{:08d}.png".format(counter), processed_img)
+        counter += 1
+    except Exception:
+        print("Image was skipped")
